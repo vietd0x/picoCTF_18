8190bcc5008e3037a36777bf6d105db675aef699
vietd0x <viet.doxv@gmail.com>
2021-02-14 21:04:34 +0700

    complete

--- reversing_warmup_2/get_flag0.py
+++ reversing_warmup_2/get_flag0.py
@@ -1,2 +1,3 @@
 #!/usr/bin/env python3
-
+import base64
+print("picoCTF{%s}" %str(base64.b64decode("dGg0dF93NHNfczFtcEwz"))[2:-1])
---
+++ reversing_warmup_2/get_flag1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "dGg0dF93NHNfczFtcEwz" | base64 -d
+# base64 -d <<< "dGg0dF93NHNfczFtcEwz"
