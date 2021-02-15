5f76bb5863c74c18b5ecadb5df217e86481e777f
vietd0x <viet.doxv@gmail.com>
2021-02-15 11:50:26 +0700

    complete

---
+++ caesar_cipher_2/get_flag.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+text = "4-'3evh?'c)7%t#e-r,g6u#.9uv#%tg2v#7g'w6gA"
+s=''
+for i in text:
+    if(ord(i) <= 65):
+        s+=chr(ord(i) + 60)
+    else:
+        s+=chr(ord(i) - 34)
+print(s)
